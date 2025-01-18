61ae51252a9650aacba4a107339647c6dc285bda
eren <eren@glitchy.com>
2025-01-18 14:03:34 +0300

    new version

---
+++ src/app/shared-metadata.ts
@@ -0,0 +1,8 @@
+export const sharedTitle = "eren.legal";
+export const sharedDescription =
+  "Self taught full-stack developer from Turkey. Making projects with 💓";
+export const sharedImage = {
+  width: 1200,
+  height: 630,
+  type: "image/png",
+};
